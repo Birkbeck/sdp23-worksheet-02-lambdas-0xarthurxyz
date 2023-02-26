84a41bc015c6e398f4417b672aa917f414e3fa6c
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-26 13:26:56 +0000

    feat: Solves Question 1.2

--- src/Outline.java
+++ src/Outline.java
@@ -15,19 +15,36 @@ public class Outline {
   }
 
   public static void question1_1() {
-    /* Question 1: 
-     * 
+    /* Question:
+     *
      * Sort the array by length (i.e., shortest to longest)
      * 
      */
     String[] arr = getArray();
-    Arrays.sort(arr, (a, b) -> Integer.compare( a.length(), b.length() ));
+    Arrays.sort(
+      arr, 
+      (a, b) -> Integer.compare( a.length(), b.length() )
+    );
     System.out.println( "Question 1.1: " + Arrays.toString(arr) );
+  }
+  
+  public static void question1_2() {
+    /* Question:
+     * 
+     * Sort the array by reverse length (i.e., longest to shortest)
+     * 
+     */
+    String[] arr = getArray();
+    Arrays.sort(
+      arr,
+      (a, b) -> Integer.compare( -a.length(), -b.length() )
+    );
+    System.out.println( "Question 1.2: " + Arrays.toString(arr) );
   }
 
   public static void main(String... args) { // varargs alternative to String[]
     System.out.println( "Input: " + Arrays.toString(getArray()) );
     question1_1();
-    
+    question1_2();
   }
 }
