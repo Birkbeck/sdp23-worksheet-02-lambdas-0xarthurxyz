f9d88591e82edca9bfa386c83cb7dbc0a89baec6
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-26 12:45:10 +0000

    refactor: Moves question 1 into own method

--- src/Outline.java
+++ src/Outline.java
@@ -2,18 +2,32 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
 
+  // Helper method
+  public static String[] getArray() {
     // Create String array
-    String[] arr = {
+    return new String[] {
       "goodbye",
       "hello",
       "good morning",
       "good afternoon"
     };
+  }
 
-    // Sort the array by length (i.e., shortest to longest)
+  public static void question1_1() {
+    /* Question 1: 
+     * 
+     * Sort the array by length (i.e., shortest to longest)
+     * 
+     */
+    String[] arr = getArray();
     Arrays.sort(arr, (a, b) -> Integer.compare( a.length(), b.length() ));
     System.out.println( "Question 1.1: " + Arrays.toString(arr) );
-  } 
+  }
+
+  public static void main(String... args) { // varargs alternative to String[]
+    System.out.println( "Input: " + Arrays.toString(getArray()) );
+    question1_1();
+    
+  }
 }
