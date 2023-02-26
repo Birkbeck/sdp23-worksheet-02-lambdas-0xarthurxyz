5ae097f07f30dde55100209cce87377d05885356
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-26 13:34:15 +0000

    feat: Solves Question 1.3

--- src/Outline.java
+++ src/Outline.java
@@ -42,9 +42,24 @@ public class Outline {
     System.out.println("Question 1.2: " + Arrays.toString(arr));
   }
 
+  public static void question1_3() {
+    /*
+     * Question:
+     * 
+     * Sort by first character
+     * 
+     */
+    String[] arr = getArray();
+    Arrays.sort(
+        arr,
+        (a, b) -> Character.compare( a.charAt(0) , b.charAt(0) ));
+    System.out.println("Question 1.3: " + Arrays.toString(arr));
+  }
+
   public static void main(String... args) { // varargs alternative to String[]
     System.out.println("Input: " + Arrays.toString(getArray()));
     question1_1();
     question1_2();
+    question1_3();
   }
 }
