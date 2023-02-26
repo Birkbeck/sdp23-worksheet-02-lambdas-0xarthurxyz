fda94a746df876584eb50ec4db4d8b8b6c478007
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-02-26 14:20:33 +0000

    feat: Solves Question 2 and minor refactor

--- src/Outline.java
+++ src/Outline.java
@@ -53,12 +53,13 @@ public class Outline {
     String[] arr = getArray();
     Arrays.sort(
         arr,
-        (a, b) -> Character.compare( a.charAt(0) , b.charAt(0) ));
+        (a, b) -> Character.compare(a.charAt(0), b.charAt(0)));
     System.out.println("Question 1.3: " + Arrays.toString(arr));
   }
 
   public static void question1_4() {
-    /* Question:
+    /*
+     * Question:
      * 
      * Sort by Strings that contain "e" first, everything else second.
      * 
@@ -67,14 +68,28 @@ public class Outline {
     Arrays.sort(
         arr,
         (a, b) -> {
-            if ( !a.contains("e") && b.contains("e") ) { return 1; } 
-            else if ( a.contains("e") && !b.contains("e") ) { return -1; } 
-            else { return 0; }
-        }
-    );
+          if (!a.contains("e") && b.contains("e")) {
+            return 1;
+          } else if (a.contains("e") && !b.contains("e")) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
     System.out.println("Question 1.4: " + Arrays.toString(arr));
   }
 
+  public static void question2() {
+    /*
+     * Question:
+     * 
+     * Sort by Strings that contain "e" first, everything else second.
+     * 
+     */
+    String[] arr = getArray();
+    Arrays.sort(arr, StringUtils::eChecker);
+    System.out.println("Question 2: " + Arrays.toString(arr));
+  }
 
   public static void main(String... args) { // varargs alternative to String[]
     System.out.println("Input: " + Arrays.toString(getArray()));
@@ -82,5 +97,6 @@ public class Outline {
     question1_2();
     question1_3();
     question1_4();
+    question2();
   }
 }
--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,3 +1,15 @@
 public class StringUtils {
-        
+        /** 
+         * Compares two strings s1 and s2 based on whether they 
+         * include the letter "e".
+         * 
+         * @param s1 One of two strings to be compared
+         * @param s2 One of two strings to be compared
+         * @return 1 if s2 contains "e" and s1 does not, -1 if s1 contains "e" and s2 does not, 0 if both do or do not contain "e".
+         */
+        public static int eChecker(String s1, String s2) {
+                if ( !s1.contains("e") && s2.contains("e") ) { return 1; } 
+            else if ( s1.contains("e") && !s2.contains("e") ) { return -1; } 
+            else { return 0; }
+        }
 }
